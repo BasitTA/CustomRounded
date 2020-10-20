ce726fcf225f83fd862e6e4d47c62d9b055c4955
Basit <bta.aang@gmail.com>
2020-10-20 14:57:00 +0700

    Initial Commit

---
+++ CustomRoundedBasitFramework/CustomRoundedBasitFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomRoundedBasitFramework.h
+//  CustomRoundedBasitFramework
+//
+//  Created by Basit Tri Anggoro on 20/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedBasitFramework.
+FOUNDATION_EXPORT double CustomRoundedBasitFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedBasitFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedBasitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedBasitFramework/PublicHeader.h>
+
+
